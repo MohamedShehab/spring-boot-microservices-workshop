4ed072be6356e340cdaf1f6f7d4684c256ffcbfb
Mohamed Shehab <midoshehab335@gmail.com>
2021-07-10 20:29:14 +0200

    First Commit

---
+++ microservice1-demo/src/main/java/com/luv2code/Microservice1DemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice1DemoApplication.class, args);
+	}
+
+}
---
+++ microservice2-demo/src/main/java/com/luv2code/Microservice2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice2DemoApplication.class, args);
+	}
+
+}
